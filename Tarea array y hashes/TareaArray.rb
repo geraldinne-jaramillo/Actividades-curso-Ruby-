dc5b0d527116bf653fc0e9a25300a91921c261e0
Geraldinne Jaramillo <57473030+geraldinne-jaramillo@users.noreply.github.com>
2023-05-11 13:33:50 -0500

    Add files via upload
    Array y hash

---
+++ Tarea array y hashes/TareaArray.rb
@@ -0,0 +1,16 @@
+ColombiaDominios = ["google","googleapis","gstatic","facebook","whatsapp","microsoft","gvt2",
+"doubleclick","googlevideo",""]
+puts ColombiaDominios[0,7]
+
+ColombiaDepartamentos={"Amazonas"=>"Leticia", "Caldas"=>"Manizales", "Vichada"=>"Puerto Carreño", "Santander"=>"Bucaramanga",
+"Vaupés"=>"Mitú", "Antioquia"=>"Medellín", "Bolivar"=>"Cartagena", "Cundinamarca"=>"Bogotá", "Córdoba"=>"Montería",
+"Boyacá"=>"Tunja", "Atlántico"=>"Barranquilla", "Arauca"=>"Arauca", "Caquetá"=>"Florencia", "Guiainía"=>"Inírida",
+"Cauca"=>"Popayán", "Chocó"=>"Quibdó", "Guaviare"=>"San José del Guaviare", "Huila"=>"Neiva", "Magdalena"=>"Santa Marta",
+"Cesar"=>"Valledupar", "Casanare"=>"Yopal", "La Guajira"=>"Riohacha", "Nariño"=>"Pasto", "Meta"=>"Villavicencio", 
+"Putumayo"=>"Mocoa", "Risaralda"=>"Pereira", "Sucre"=>"Sincelejo", "Tolima"=>"Ibagué", "Valle del cauca"=>"Cali", "Quindío"=>"Armenia",
+"San Andrés y Providencia"=>"San Andrés", "Norte de Santander"=>"Cúcuta"}
+puts ColombiaDepartamentos.size()
+
+SistemasAutonomos={"AS3816" => "COLOMBIA TELECOMUNICACIONES S.A. ESP", "AS13489" => "EPM TELECOMUNICACIONES S.A. E.S.P.",
+"AS10620" => "TELMEX COLOMBIA S.A.", "AS19429" => "ETB-COLOMBIA", "AS27831" => "COLOMBIA MOVIL"}
+puts SistemasAutonomos
